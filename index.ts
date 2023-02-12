7e32cc360cd63261c24498241c45e593a1e2cde4
francanete <francanetedev@gmail.com>
2023-02-12 22:03:29 +0000

    cleanup code

--- index.ts
+++ index.ts
@@ -5,16 +5,18 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hola World!");
 });
 
 app.get("/api", (req, res) => {
   res.send({
-    name: "Fran",
-    age: 30,
+    name: "Yoda",
+    age: 900,
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
